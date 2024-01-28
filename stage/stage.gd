1a710735429b7bf64f4dec209da4880e9351040c
vodpox <edvinas.senda@gmail.com>
2024-01-28 11:59:28 +0200

    act freezing

--- stage/stage.gd
+++ stage/stage.gd
@@ -6,8 +6,23 @@ var current_act: Node2D
 
 signal finished_act(succeeded: bool)
 
+
+var act_frozen := false
+
+
 func _ready():
 	change_act()
+
+func _input(event):
+	return
+	
+	if event.is_action_pressed("jump"):
+		if act_frozen:
+			change_act()
+			print('c')
+		else:
+			freeze_act()
+			print('f')
 
 func change_act():
 	if current_act != null:
@@ -17,6 +32,11 @@ func change_act():
 	add_child(current_act)
 	
 	current_act.finished_act.connect(_on_finished_act)
+	
+	if $Timer.is_stopped():
+		$Timer.start()
+	
+	act_frozen = false
 
 
 func _on_timer_timeout():
@@ -24,3 +44,8 @@ func _on_timer_timeout():
 
 func _on_finished_act(succeeded: bool):
 	finished_act.emit(succeeded)
+
+func freeze_act():
+	act_frozen= true
+	
+	$Timer.stop()
