587359204411881558ef07eb8fe6cbbe95abf1ca
vodpox <edvinas.senda@gmail.com>
2024-01-28 13:30:50 +0200

    cool faces

--- audience_person.gd
+++ audience_person.gd
@@ -2,8 +2,8 @@ class_name AudiencePerson
 extends Sprite2D
 
 @export var texture_clapping: Texture2D
-@export var texture_neutral: Texture2D
-@export var texture_laughing: Texture2D
+@export var texture_neutral: Array[Texture2D]
+@export var texture_laughing: Array[Texture2D]
 
 enum STATE {
 	CLAPPING,
@@ -16,8 +16,49 @@ var state: STATE = STATE.NEUTRAL:
 		state = st
 		match st:
 			STATE.CLAPPING:
-				texture = texture_clapping
+				clap_face()
 			STATE.NEUTRAL:
-				texture = texture_neutral
+				neutral_face()
 			STATE.LAUGHING:
-				texture = texture_laughing
+				laugh_face()
+
+
+func _ready():
+	self.state = STATE.NEUTRAL
+	
+	$FaceAnimTimer.wait_time = 1 + randf()
+	
+	$AnimationPlayer.current_animation = 'clap'
+
+
+func _on_timer_timeout():
+	if state == STATE.NEUTRAL:
+		neutral_face()
+
+
+func neutral_face():
+	texture = texture_neutral.pick_random()
+	if randf() > 0:
+		transform.x *= -1
+	
+	$FaceClapHandLeft.visible = false
+	$FaceClapHandRight.visible = false
+
+
+func clap_face():
+	texture = texture_clapping
+	
+	$FaceClapHandLeft.visible = true
+	$FaceClapHandRight.visible = true
+
+
+func laugh_face():
+	texture = texture_laughing.pick_random()
+	
+	$FaceClapHandLeft.visible = false
+	$FaceClapHandRight.visible = false
+
+
+func _on_laugh_timer_timeout():
+	if state == STATE.LAUGHING:
+		texture = texture_laughing.pick_random()
