5c951beb5e60f72b92d32b0f8d94b750c60da57e
Uxeron <vgabrielius123@gmail.com>
2024-08-05 22:26:37 +0300

    Remove solana

--- menu/main_menu.gd
+++ menu/main_menu.gd
@@ -1,7 +1,6 @@
 extends Panel
 
 @export var button_start: Button
-@export var button_login: Button
 @export var button_credits: Button
 @export var button_quit: Button
 
@@ -11,11 +10,9 @@ var game: Node = preload("res://main.tscn").instantiate()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	SolanaService.wallet.connect("on_logged_in",confirm_login)
 	game.get_child(1).menu = self
 	
 	button_start.pressed.connect(func(): start_game())
-	button_login.pressed.connect(func(): SolanaService.wallet.try_login())
 	button_credits.pressed.connect(func(): pass)
 	button_quit.pressed.connect(func(): get_tree().quit())
 	
@@ -28,12 +25,4 @@ func start_game():
 	get_tree().current_scene = game
 	get_parent().remove_child(self)
 
-func confirm_login(login_success:bool) -> void:
-	if login_success:
-		var wallet_address = SolanaService.wallet.get_pubkey().get_value()
-		button_login.text = "Login success!"
-		button_login.disabled = true
-		label_address.text = "Wallet address: " + wallet_address
-	else:
-		print("Failed")
 
